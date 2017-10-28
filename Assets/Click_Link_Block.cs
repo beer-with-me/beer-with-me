6ba53d88641bf62bb4192954d0555149d4be6ab8
charlie <charlie275691502@gmail.com>
2017-10-29 02:07:00 +0800

    complete link_manager.cs

---
+++ Assets/Click_Link_Block.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click_Link_Block : MonoBehaviour {
+	public LinkDevice_Manager linkDevice_Manager;
+	public int order;
+
+	void OnMouseUp(){
+		StartCoroutine(linkDevice_Manager.Press_Link_Buttons(order));
+	}
+}
--- Assets/_Script/_Phases/LinkDevice/LinkDevice_Manager.cs
+++ Assets/_Script/_Phases/LinkDevice/LinkDevice_Manager.cs
@@ -7,16 +7,33 @@ public class LinkDevice_Manager : MonoBehaviour {
 	public GameController gameController;
 	public NetworkController networkController;
 	public Text room_ID_text;
+	public Color[] colors;
+	public GameObject[] link_Blocks;
 
 	void OnEnable () {
 		room_ID_text.text = gameController.room_ID.ToString();
 	}
 
-	public void Press_Link_Buttons(){
-		// 	若此邊尚未連接
-		//		建立連接
+	public IEnumerator Press_Link_Buttons(int order){
+		// 	若此邊尚未連接 -> 建立連接
+		networkController.SendToServer(new Pocket(gameController.version, C2M_Command.C2M_LINK_KEY, new int[1]{order}));
 
-		// 	若此邊已連接
-		//		取消連接
+		float connect_time = 0.0f;
+		while (networkController.now_Pocket == null) {
+			connect_time += Time.deltaTime;
+			yield return null;
+		}
+
+		int dir = networkController.now_Pocket.datas [0];
+		int color_index = networkController.now_Pocket.datas [1];
+		networkController.now_Pocket = null;
+
+		Change_Color (dir, colors[color_index]);
+
+		// 	若此邊已連接 -> 取消連接
+	}
+
+	public void Change_Color(int dir, Color color){
+		link_Blocks [dir - 1].GetComponent<SpriteRenderer> ().color = color;
 	}
 }
