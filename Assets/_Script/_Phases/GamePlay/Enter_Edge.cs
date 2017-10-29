2cf6209b1900e67395c562cf1cf10b7a43edf0a9
JerryShen <jerry82422@gmail.com>
2017-10-29 22:41:15 +0800

    Add `Edge` for `C2M_CROSS`

--- Assets/_Script/_Phases/GamePlay/Enter_Edge.cs
+++ Assets/_Script/_Phases/GamePlay/Enter_Edge.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enter_Edge : MonoBehaviour {
+	public int dirKey;
 	public GamePlay_Manager gamePlay_manager;
 
-	void OnCollisionExit(Collision collisionInfo) {
+	void OnCollisionEnter(Collision collisionInfo) {
 		// Falling off the table / entering the other table
-		if (collisionInfo.gameObject.name == "Table") {
-			gamePlay_manager.LeaveTable ();
+		if (collisionInfo.gameObject.name == "Beer") {
+			gamePlay_manager.LeaveTable (dirKey);
 		}
 	}
 }
--- Assets/_Script/_Phases/GamePlay/GamePlay_Manager.cs
+++ Assets/_Script/_Phases/GamePlay/GamePlay_Manager.cs
@@ -11,7 +11,8 @@ public class GamePlay_Manager : MonoBehaviour {
 		
 	}
 
-	public void LeaveTable() {
-		networkController.SendToServer (new Pocket (gameController.version, C2M_Command.C2M_CROSS, new int[6]{1, 1, 1, 1, 1, 1}));
+	public void LeaveTable(int dirKey) {
+		Debug.Log ("Edge_" + dirKey.ToString() + " entered.");
+		networkController.SendToServer (new Pocket (gameController.version, C2M_Command.C2M_CROSS, new int[6]{dirKey, 1, 1, 1, 1, 1}));
 	}
 }
