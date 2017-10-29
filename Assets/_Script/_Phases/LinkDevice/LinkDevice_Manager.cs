8923850f9c6f5bed43688fec12e2cb4dfba2aca6
charlie <charlie275691502@gmail.com>
2017-10-29 10:41:56 +0800

    fixed sending data error

--- Assets/_Script/_Phases/LinkDevice/LinkDevice_Manager.cs
+++ Assets/_Script/_Phases/LinkDevice/LinkDevice_Manager.cs
@@ -36,4 +36,23 @@ public class LinkDevice_Manager : MonoBehaviour {
 	public void Change_Color(int dir, Color color){
 		link_Blocks [dir - 1].GetComponent<SpriteRenderer> ().color = color;
 	}
+
+	public IEnumerator Start0(){
+		// 建立 start0 賽局
+		networkController.SendToServer(new Pocket(gameController.version, C2M_Command.C2M_START0, new int[0]{}));
+
+		float connect_time = 0.0f;
+		while (networkController.now_Pocket == null) {
+			connect_time += Time.deltaTime;
+			yield return null;
+		}
+
+		gameController.start_Here = (networkController.now_Pocket.datas [0] == 0) ? false : true;
+		gameController.start_Position = new Vector2(networkController.now_Pocket.datas [1], networkController.now_Pocket.datas [2]);
+		networkController.now_Pocket = null;
+
+
+		// 向 GameController 回報階段任務完成 ， disable script
+		gameController.SwitchPhases(Phases.GamePlay);
+	}
 }
